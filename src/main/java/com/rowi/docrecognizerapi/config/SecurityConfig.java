4aecf0cec0f5ba9f586dc336b0260ff99eccf62e
Ivan Afanasev <yamakassi451@gmail.com>
2023-07-04 21:01:13 +0400

    add keycloak adapter

--- src/main/java/com/rowi/docrecognizerapi/config/SecurityConfig.java
+++ src/main/java/com/rowi/docrecognizerapi/config/SecurityConfig.java
@@ -1,55 +1,55 @@
 package com.rowi.docrecognizerapi.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig   {
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    private  String  issuerUri;
-    private final JwtAuthConverter jwtAuthConverter;
-
-    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
-        this.jwtAuthConverter = jwtAuthConverter;
-    }
-/*
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration conf = new CorsConfiguration();
-        conf.setAllowedOriginPatterns(Collections.singletonList("*"));
-        conf.setAllowedMethods(Arrays.asList("GET", "POST","PATCH",DE));
-        conf.setAllowCredentials(true);
-        conf.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", conf);
-        return source;
-    }*/
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("*")
-                .hasRole("USER")
-                .anyRequest()
-                .permitAll()
-                .and()
-                .oauth2ResourceServer()
-                .jwt()
-                .jwtAuthenticationConverter(jwtAuthConverter);
-
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        return http.build();
+@KeycloakConfiguration
+@Import(KeycloakSpringBootConfigResolver.class)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider = new KeycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
     }
 
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
+    }
 
+    @Bean
+    protected SessionRegistry buildSessionRegistry() {
+        return new SessionRegistryImpl();
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
 
-
-
+        http.cors().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/api/*").hasRole("USER");
+        http.authorizeRequests().anyRequest().permitAll();
+    }
 }
+
--- src/main/java/com/rowi/docrecognizerapi/controller/KeycloakTestController.java
+++ src/main/java/com/rowi/docrecognizerapi/controller/KeycloakTestController.java
@@ -1,23 +1,20 @@
 package com.rowi.docrecognizerapi.controller;
 
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api")
 public class KeycloakTestController {
 
     @GetMapping("/me")
-    public Authentication whoAmI() {
-        SecurityContext context = SecurityContextHolder.getContext();
-        System.out.println(context.getAuthentication());
-        return context.getAuthentication();
+    public String getCurrentUser(
+            KeycloakAuthenticationToken token
+    ) {
+        return token.getName();
     }
-
 }
-
